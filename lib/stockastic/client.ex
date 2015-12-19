0d719208cc5b8a6e6bcd9353dab122edd2db050a
Kirill Pimenov <kirill@pimenov.cc>
2015-12-19 18:53:37 +0100

    Smarter path joining

--- lib/stockastic/client.ex
+++ lib/stockastic/client.ex
@@ -12,11 +12,6 @@ defmodule Stockastic.Client do
 
   @spec new(auth, binary) :: t
   def new(auth, endpoint) do
-    endpoint = if String.ends_with?(endpoint, "/") do
-      endpoint
-    else
-      endpoint <> "/"
-    end
     %__MODULE__{auth: auth, endpoint: endpoint}
   end
 end
